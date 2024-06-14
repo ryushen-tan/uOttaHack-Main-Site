590496db6b729f610b385bad29ce8d21a345b92b
ryushen-tan <78460980+ryushen-tan@users.noreply.github.com>
2024-06-14 08:33:57 -0400

    Fix: Made text more legible

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Description = () => {
     return (
         <div className='flex flex-col font-semibold mt-[6vw] text-center justify-center Roboto w-full'>
-            <h1 className='text-[64px] font-semibold'>What is uOttaHack?</h1>
+            <h1 className='text-[54px] font-semibold'>What is uOttaHack?</h1>
             <div className='w-[65vw] mx-auto'>
-                <p className='font-medium Roboto text-[32px] mt-[2vw]'>uOttaHack is Ottawa’s Premier Hackathon community since 2017. It is organized by a student group directly with the University of Ottawa's Faculty of Engineering and the President and Vice-Chancellor's team to deliver Ottawa's largest annual technology.</p>
+                <p className='font-medium Roboto text-[22px] mt-[2vw]'>uOttaHack is Ottawa’s Premier Hackathon community since 2017. It is organized by a student group directly with the University of Ottawa's Faculty of Engineering and the President and Vice-Chancellor's team to deliver Ottawa's largest annual technology.</p>
             </div>
         </div>
     );
